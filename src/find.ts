67dc84ec088bd6543f34121d3a7386619b5f148f
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-09 10:11:34 +0600

    find operation via prisma into postgresql. like findMany, findFirst, findFirstOrThrow, findUnique, findUniqueOrThrow

---
+++ src/find.ts
@@ -0,0 +1,40 @@
+import { PrismaClient } from "../generated/prisma";
+
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // find all.
+  const getAllFromDB = await prisma.post.findMany();
+
+  // findFirst -> find by id. find single.
+  const findFirst = await prisma.post.findFirst({
+    where: {
+      id: 1,
+    },
+  });
+
+  // findFirstOrThrow - find single.
+  //   const findFirstOrThrow = await prisma.post.findFirstOrThrow({
+  //     where: {
+  //       id: 10,
+  //     },
+  //   });
+
+  // findUnique - find by unique field. find single.
+  const findUnique = await prisma.post.findUnique({
+    where: {
+      id: 1,
+    },
+  });
+
+  // findUniqueOrThrow - find by unique field. find single.
+  const findUniqueOrThrow = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: 10,
+    },
+  });
+
+  console.log(findUniqueOrThrow);
+};
+
+main();
