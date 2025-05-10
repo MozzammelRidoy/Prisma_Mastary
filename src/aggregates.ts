966a3ce6a736aa066af2cc44057ef7ab8ec9a628
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-10 11:24:52 +0600

    find sum, count, max, min and total record count usnig aggregates

--- src/aggregates.ts
+++ src/aggregates.ts
@@ -3,12 +3,44 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const main = async () => {
+  // find avarage.
   const avgAge = await prisma.user.aggregate({
     _avg: {
       age: true,
     },
   });
-  console.log(avgAge);
+
+  // find sum of age.
+  const sumAge = await prisma.user.aggregate({
+    _sum: {
+      age: true,
+    },
+  });
+
+  // find count.
+  const countAge = await prisma.user.aggregate({
+    _count: {
+      age: true,
+    },
+  });
+
+  // find number of record..
+  const countData = await prisma.user.count();
+
+  // find max age.
+  const maxAge = await prisma.user.aggregate({
+    _max: {
+      age: true,
+    },
+  });
+
+  // find min age.
+  const minAge = await prisma.user.aggregate({
+    _min: {
+      age: true,
+    },
+  });
+  console.log(minAge);
 };
 
 main();
