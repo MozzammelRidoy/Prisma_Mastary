b4f5aa8b534374044693465facd423c914866ace
MozzammelRidoy <mozzammelridoy5iii@gmail.com>
2025-05-10 13:41:42 +0600

    prisma helpful command

---
+++ src/command.ts
@@ -0,0 +1,8 @@
+// npx prisma migrate dev
+// npx prisma generate
+// npx prisma validate
+// npx prisma format
+// npx prisma db pull --print
+// npx prisma db pull
+// npx prisma db push
+// npx prisma
